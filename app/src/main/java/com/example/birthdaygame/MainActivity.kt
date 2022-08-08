d8ed71b0ac63704081589fe6e90e8d8028ae9bdc
doidoe <limoh19@naver.com>
2022-08-08 10:53:53 +0900

    dkjfslk

--- app/src/main/java/com/example/birthdaygame/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygame/MainActivity.kt
@@ -8,7 +8,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        //super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         nextbutton.setOnClickListener{
